a958789c99757bd00a24ec3cd5c820865b1fa55f
Chim-SO <chimso1994@gmail.com>
2022-12-19 15:56:43 +0100

    Explore the original dataset idx (format).

---
+++ dataset_scripts/explore_dataset_idx.py
@@ -0,0 +1,26 @@
+import numpy as np
+import idx2numpy
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    # Files location:
+    train_or_test = 'train'  # train or t10k
+    labels_file = f'dataset_idx/{train_or_test}-labels.idx1-ubyte'
+    images_file = f'dataset_idx/{train_or_test}-images.idx3-ubyte'
+
+    # Read data:
+    labels_data = idx2numpy.convert_from_file(labels_file)
+    images_data = idx2numpy.convert_from_file(images_file)
+
+    # Display number of instances:
+    n_images, im_size = images_data.shape[:2]
+    n_labels = labels_data.shape[0]
+    print(f"There is {n_images} images.")
+    print(f"There is {n_labels} labels.")
+    print(f"The images size is {im_size}.")
+
+    # Display an element:
+    element = 10
+    plt.imshow(images_data[element], cmap=plt.cm.binary)
+    plt.title(labels_data[element])
+    plt.show()
