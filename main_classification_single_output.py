0fa7a4d4ede3367bcda31e581bc8f257f5258402
Chim-SO <chimso1994@gmail.com>
2023-01-12 11:34:41 +0100

    Use keras dataset.

--- main_classification_single_output.py
+++ main_classification_single_output.py
@@ -20,27 +20,32 @@ import numpy as np
 from sklearn.metrics import accuracy_score, recall_score, precision_score, ConfusionMatrixDisplay
 from sklearn.model_selection import train_test_split
 
-
-def split_dataset(dataset, train_frac=0.7):
-    train = dataset.sample(frac=train_frac)
-    val = dataset.drop(train.index)
-    return train, val
-
-
 if __name__ == '__main__':
-    # read dataset:
-    df = pd.read_csv('dataset_csv/train.csv')
+    # Read dataset:
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+    print(f"The training data shape: {x_train.shape}, its label shape: {y_train.shape}")
+    print(f"The test data shape: {x_test.shape}, its label shape: {y_test.shape}")
 
-    # split dataset to train validation:
-    train, val = split_dataset(df)
-    x_train, y_train = train.drop('label', axis=1) / 255., train['label']
-    x_val, y_val = val.drop('label', axis=1) / 255., val['label']
+    # Dimension transformation:
+    x_train = x_train.reshape((x_train.shape[0], x_train.shape[1] * x_train.shape[2]))
+    x_test = x_test.reshape((x_test.shape[0], x_test.shape[1] * x_test.shape[2]))
+    print(f"The training data shape becomes: {x_train.shape}, its label shape: {y_train.shape}")
+    print(f"The test data shape becomes: {x_test.shape}, its label shape: {y_test.shape}")
+
+    # Preprocessing: scaling:
+    x_train = x_train.astype("float32") / 255
+    x_test = x_test.astype("float32") / 255
+
+    # Split dataset:
+    x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.3, random_state=42)
 
     # Display bars:
     fig, axs = plt.subplots(1, 2)
-    axs[0].bar([str(_) for _ in range(10)], train['label'].value_counts(), width=0.4)
+    unique, counts = np.unique(y_train, return_counts=True)
+    axs[0].bar(unique, counts, width=0.4)
     axs[0].set_title('Train set')
-    axs[1].bar([str(_) for _ in range(10)], val['label'].value_counts(), width=0.4)
+    unique, counts = np.unique(y_val, return_counts=True)
+    axs[1].bar(unique, counts, width=0.4)
     axs[1].set_title('Validation set')
     plt.show()
 
@@ -82,31 +87,29 @@ if __name__ == '__main__':
     plt.show()
 
     # Evaluation:
-    test_df = pd.read_csv('dataset_csv/test.csv')
-    x_test, y_test = test_df.drop('label', axis=1) / 255., test_df['label']
     test_results = model.evaluate(x_test, y_test, verbose=1)
     print(f'Test set: - loss: {test_results[0]} - {metric}: {test_results[1]}')
 
     # Classification evaluation:
-    train_pred = np.rint(np.clip(model.predict(x_train), 0, 9))
-    val_pred = np.rint(np.clip(model.predict(x_val), 0, 9))
-    test_pred = np.rint(np.clip(model.predict(x_test), 0, 9))
+    pred_train = np.rint(np.clip(model.predict(x_train), 0, 9))
+    pred_val = np.rint(np.clip(model.predict(x_val), 0, 9))
+    pred_test = np.rint(np.clip(model.predict(x_test), 0, 9))
     print("Displaying other metrics:")
     print("\t\tAccuracy (%)\tPrecision (%)\tRecall (%)")
     print(
-        f"Train:\t{round(accuracy_score(y_train, train_pred, normalize=True) * 100, 2)}\t\t\t"
-        f"{round(precision_score(y_train, train_pred, average='macro') * 100, 2)}\t\t\t"
-        f"{round(recall_score(y_train, train_pred, average='macro') * 100, 2)}")
+        f"Train:\t{round(accuracy_score(y_train, pred_train, normalize=True) * 100, 2)}\t\t\t"
+        f"{round(precision_score(y_train, pred_train, average='macro') * 100, 2)}\t\t\t"
+        f"{round(recall_score(y_train, pred_train, average='macro') * 100, 2)}")
     print(
-        f"Val :\t{round(accuracy_score(y_val, val_pred, normalize=True) * 100, 2)}\t\t\t"
-        f"{round(precision_score(y_val, val_pred, average='macro') * 100, 2)}\t\t\t"
-        f"{round(recall_score(y_val, val_pred, average='macro') * 100, 2)}")
+        f"Val :\t{round(accuracy_score(y_val, pred_val, normalize=True) * 100, 2)}\t\t\t"
+        f"{round(precision_score(y_val, pred_val, average='macro') * 100, 2)}\t\t\t"
+        f"{round(recall_score(y_val, pred_val, average='macro') * 100, 2)}")
     print(
-        f"Test:\t{round(accuracy_score(y_test, test_pred, normalize=True) * 100, 2)}\t\t\t"
-        f"{round(precision_score(y_test, test_pred, average='macro') * 100, 2)}\t\t\t"
-        f"{round(recall_score(y_test, test_pred, average='macro') * 100, 2)}")
+        f"Test:\t{round(accuracy_score(y_test, pred_test, normalize=True) * 100, 2)}\t\t\t"
+        f"{round(precision_score(y_test, pred_test, average='macro') * 100, 2)}\t\t\t"
+        f"{round(recall_score(y_test, pred_test, average='macro') * 100, 2)}")
 
     # Confusion matrix:
-    ConfusionMatrixDisplay.from_predictions(val['label'], val_pred, normalize='true')
+    ConfusionMatrixDisplay.from_predictions(y_val, pred_val, normalize='true')
     plt.savefig('output/single/confmat.png', bbox_inches='tight')
     plt.show()
